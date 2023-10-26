7ca034fa5fa49038d79bbc425e3db8fc96bc2ab9
nickxiong2030innsx@gmail.com <kou20.xiong21@gmail.com>
2023-10-26 15:08:23 -0400

    End of Section 8 Angular Basics & Nav-Bar commit

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'SkiNet E-Commerce App';
+  products: IProduct[] = [];
+
+  constructor(private http: HttpClient) {    
+  }
+
+  ngOnInit(): void {
+    this.http.get<IPagination>('https://localhost:5001/api/products').subscribe((response: IPagination) => {
+      console.log("Responses: ", response);
+      this.products = response.data;
+    }, error => {
+      console.log("Error: ", error);
+    });
+  }
+
 }
